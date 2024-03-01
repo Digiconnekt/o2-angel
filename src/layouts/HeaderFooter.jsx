15ee499be2c166a7b91b7646085baae5e08fc583
Rohan Daddekar <rohandaddekar9@gmail.com>
2024-03-01 17:37:19 +0530

    scrolltotop added

--- src/layouts/HeaderFooter.jsx
+++ src/layouts/HeaderFooter.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import ScrollToTop from "../components/ScrollToTop/ScrollToTop";
 
 const HeaderFooter = () => {
   return (
     <>
+      <ScrollToTop />
+
       <Header />
       <Outlet />
       <Footer />
